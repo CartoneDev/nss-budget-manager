5a538b439a9db17c36b6cf95830c4b9c56063a49
Tsay, Vyacheslav <tsayvyac@fel.cvut.cz>
2023-05-07 18:50:18 +0200

    Created main screen

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import MainScreen from "./pages/MainScreen";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Login />}/>
                 <Route path="/registration" element={<Registration />}/>
+                <Route path="/budget" element={<MainScreen />}/>
             </Routes>
         </BrowserRouter>
     );
