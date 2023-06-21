d5a1b2a914b1724cdd1506ffa934a4f4479b7622
lezhnevg <lezhnevg@fel.cvut.cz>
2023-06-21 15:37:38 +0200

    refactor dashboard page

--- fe/src/pages/Dashboard.jsx
+++ fe/src/pages/Dashboard.jsx
@@ -3,11 +3,13 @@ import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
+import Button from "@mui/material/Button";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import Table from "@mui/material/Table";
 import { createTheme, ThemeProvider } from "@mui/material";
-import './styles/Wallet.css';
 
 const theme = createTheme();
 
@@ -18,6 +20,8 @@ const initialState = {
     name: '',
     budget_limit: null,
     currency: '',
+    amountGoal: '',
+    nameGoal: '',
 };
 
 const reducer = (state, action) => {
@@ -25,11 +29,9 @@ const reducer = (state, action) => {
         case 'SET_WALLET':
             return { ...state, ...action.payload };
         case 'SET_CURRENCY':
-            return { ...state, currency: action.payload.currency };
-        case 'SET_BUDGET_LIMIT':
-            return { ...state, budget_limit: action.payload };
-        case 'SET_AMOUNT':
-            return { ...state, amount: action.payload };
+            return { ...state, currency: action.payload.currency, budget_limit: action.payload.budget_limit, amount: action.payload.amount };
+        case 'SET_GOALS':
+            return { ...state, amountGoal: action.payload.amountGoal, NameGoal: action.payload.NameGoal };
         default:
             return state;
     }
@@ -37,13 +39,17 @@ const reducer = (state, action) => {
 
 const Dashboard = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const [isInitialLoad, setIsInitialLoad] = useState(true);
     const [currencyChange, setCurrencyChange] = useState('CZK');
+    const [showGoals, setShowGoals] = useState(false); // New state variable for showing goals
+    const [showTransactions, setShowTransactions] = useState(false);
+
 
     useEffect(() => {
-        // Tento kód se spustí při načtení stránky
         const storedState = localStorage.getItem('walletState');
         if (storedState) {
             dispatch({ type: 'SET_WALLET', payload: JSON.parse(storedState) });
+            setIsInitialLoad(false); // Set initial load to false if data exists in localStorage
         } else {
             fetchWalletData()
                 .then((data) => {
@@ -52,21 +58,20 @@ const Dashboard = () => {
                     } else {
                         console.log('Wallet does not exist in the database.');
                     }
+                    setIsInitialLoad(false); // Set initial load to false after fetching data
                 })
                 .catch((error) => {
                     console.log('Error loading data from the database:', error);
+                    setIsInitialLoad(false); // Set initial load to false if error occurs
                 });
         }
-
-        // Tento kód se spustí při změně hodnoty currencyChange
-        updateBudgetLimitAndAmount();
-    });
+    }, []);
 
     const fetchWalletData = async () => {
         try {
             const response = await fetch('/api/wallet');
             if (response.ok) {
-                return await response.json();;
+                return await response.json();
             } else {
                 throw new Error('Invalid response from the server.');
             }
@@ -75,18 +80,22 @@ const Dashboard = () => {
         }
     };
 
-    const handleCurrencyChange = (event) => {
+    const handleCurrencyChange = async (event) => {
         setCurrencyChange(event.target.value);
-    };
-
-    const updateBudgetLimitAndAmount = async () => {
+        const newCurrency = event.target.value;
         try {
-            const budgetLimit = await getBudgetLimitForCurrency(currencyChange);
-            const amount = await getAmountForCurrency(currencyChange);
-            dispatch({ type: 'SET_BUDGET_LIMIT', payload: budgetLimit });
-            dispatch({ type: 'SET_AMOUNT', payload: amount });
+            const budgetLimit = await getBudgetLimitForCurrency(newCurrency);
+            const amount = await getAmountForCurrency(newCurrency);
+            dispatch({
+                type: 'SET_CURRENCY',
+                payload: {
+                    currency: newCurrency,
+                    budget_limit: budgetLimit,
+                    amount: amount,
+                },
+            });
         } catch (error) {
-            console.log('Error while updating budget limit and amount:', error);
+            console.log('Error while changing the currency:', error);
         }
     };
 
@@ -106,8 +115,16 @@ const Dashboard = () => {
             const data = await response.json();
             return data.amount;
         } catch (error) {
-            throw new Error('Error while retrieving the amount:', error);
+            throw new Error('Error while retrieving the total income:', error);
         }
+    };
+
+    const handleGoalsClick = () => {
+        setShowGoals(!showGoals); // Toggle the showGoals state on button click
+    };
+
+    const handleShowTransactions = () => {
+        setShowTransactions(!showTransactions);
     };
 
     return (
@@ -125,34 +142,89 @@ const Dashboard = () => {
                 >
                     <Box
                         sx={{
-                            border: '1px',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            border: '1px solid #ccc',
                             borderRadius: '24px',
                             boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
                             padding: '16px',
-                            margin: '10 auto',
+                            maxWidth: '400px',
+                            margin: '0 auto',
                         }}
                     >
-                        <RadioGroup
-                            row
-                            aria-label="currency"
-                            name="currency"
-                            value={currencyChange}
-                            onChange={handleCurrencyChange}
-                            sx={{ padding: '16px' }}
-                        >
-                            <FormControlLabel value="CZK" control={<Radio />} label="CZK" />
-                            <FormControlLabel value="USD" control={<Radio />} label="USD" />
-                            <FormControlLabel value="EUR" control={<Radio />} label="EUR" />
-                        </RadioGroup>
-                        <Typography component="h1" variant="h5">
-                            <strong>Wallet name:</strong> {state.name}
-                        </Typography>
-                        <Typography component="p">
-                            <strong>Budget Limit:</strong> {state.budget_limit} {state.currency}
-                        </Typography>
-                        <Typography component="p">
-                            <strong>Amount:</strong> {state.amount} {state.currency}
-                        </Typography>
+                        {isInitialLoad ? (
+                            <Typography variant="h6">Loading...</Typography>
+                        ) : (
+                            <>
+                                <Typography variant="h6">Selected Currency: {currencyChange}</Typography>
+                                <Typography component="h1" variant="h5">
+                                    <strong>Wallet name:</strong> {state.name}
+                                </Typography>
+                                <Typography component="p">
+                                    <strong>Budget Limit:</strong> {state.budget_limit} {state.currency}
+                                </Typography>
+                                <Typography component="p">
+                                    <strong>Amount:</strong> {state.amount} {state.currency}
+                                </Typography>
+                            </>
+                        )}
+                    </Box>
+                    <Box
+                        sx={{
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            border: '1px solid #ccc',
+                            borderRadius: '24px',
+                            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+                            padding: '16px',
+                            maxWidth: '400px',
+                            margin: '0 auto',
+                            marginTop: '16px', // Add margin top to separate from the wallet information box
+                        }}
+                    >
+                        {/* ... Existing JSX code ... */}
+                        <Button variant="outlined" onClick={handleGoalsClick} sx={{ marginTop: '16px' }}>
+                            Goals
+                        </Button>
+                        {showGoals && (
+                            <Typography component="p" sx={{ marginTop: '8px' }}>
+                                <strong>Goal Name:</strong> {state.NameGoal}
+                                <strong>Goal Amount:</strong> {state.amountGoal}
+                            </Typography>
+                        )}
+                    </Box>
+                    <Box
+                        sx={{
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            border: '1px solid #ccc',
+                            borderRadius: '24px',
+                            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+                            padding: '16px',
+                            margin: '0 auto',
+                            marginTop: '16px', // Add margin top to separate from the wallet information box
+                        }}
+                    >
+                        <Button variant="contained" onClick={handleShowTransactions}>
+                            {showTransactions ? 'Hide Transactions' : 'Show Transactions'}
+                        </Button>
+                        {showTransactions && (
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Description</TableCell>
+                                        <TableCell>ID</TableCell>
+                                        <TableCell>Date</TableCell>
+                                        <TableCell>Money</TableCell>
+                                        <TableCell>Category</TableCell>
+                                        <TableCell>Type</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {/* Render transactions here */}
+                                </TableBody>
+                            </Table>
+                        )}
                     </Box>
                 </Box>
             </Container>
